2763978f68804b6c4fcb3e159722bf65eadc668a
DoroDore <tlyz82@student.cis.eduhk>
2024-02-06 10:57:46 +0800

    First in the class

--- src/Address.java
+++ src/Address.java
@@ -1,2 +1,32 @@
 public class Address {
+    private String district;
+    private String street;
+    private int streetNumber;
+    public Address(String district, String street, int streetNumber) {
+        this.district = district;
+        this.street = street;
+        this.streetNumber = streetNumber;
+    }
+    public String getDistrict(){
+        return district;
+    }
+    public String getStreet() {
+        return street;
+    }
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+    public void setDistrict(String str) {
+        district = str;
+    }
+    public void setStreet(String str) {
+        street = str;
+    }
+    public void setStreetNumber(int n) {
+        streetNumber = n;
+    }
+    @Override
+    public String toString() {
+        return (streetNumber + " " + street + ", " + district);
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,55 @@
 public class Student {
+    private int age;
+    private String firstName;
+    private String lastName;
+    private int year;
+    private Address address;
+    public Student(int age, String firstName, String lastName, int year, Address address) {
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.year = year;
+        this.address = address;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public int getYear() {
+        return year;
+    }
+    public Address getAddress() {
+        return address;
+    }
+    public void setAge(int n) {
+        age = n;
+    }
+    public void setFirstName(String str) {
+        firstName = str;
+    }
+    public void setLastName(String str) {
+        lastName = str;
+    }
+    public void setYear(int n) {
+        year = n;
+    }
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+    private void addOneAge() {
+        age++;
+    }
+    public int birthday() {
+        addOneAge();
+        return age;
+    }
+    @Override
+    public String toString() {
+        return (firstName + " " + lastName + ", age " + age + ", year " + year + ", " + address.toString());
+    }
 }
